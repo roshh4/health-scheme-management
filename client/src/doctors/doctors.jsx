a3633fe3a4160b10c820718ff914d4d6ffbd0e5f
roshh4 <roshinidharmaraj@gmail.com>
2024-08-30 09:39:32 +0530

    fixed some stuff

--- client/src/doctors/doctors.jsx
+++ client/src/doctors/doctors.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import "./doctors.css";
 
 function Doctor() {
   const [doctors, setDoctors] = useState([]);
@@ -14,22 +15,32 @@ function Doctor() {
           });
   }, []);
 
+  const getDoctorinfo = () => {
+    console.log(`Button clicked for doctor with ID`);
+  }
+
   return (
       <div className="App">
           <h1>Doctor Directory</h1>
-          <div>
+          <ul className="doctor-list">
               {doctors.map(doctor => (
-                  <div key={doctor._id}>
-                      <img src={doctor.profile_picture_url} alt={`${doctor.first_name} ${doctor.last_name}`} />
-                      <h2>{doctor.first_name} {doctor.last_name}</h2>
-                      <p>Specialization: {doctor.specialization}</p>
-                      <p>Experience: {doctor.years_of_experience} years</p>
-                      <p>Department: {doctor.department}</p>
-                      <p>Consultation Fees: Rs. {doctor.consultation_fees}</p>
-                      <p>Biography: {doctor.biography}</p>
-                  </div>
+                  <li key={doctor._id} className="doctor-list-item">
+                      <div>
+                          <div className="doctor-info">
+                              <h2>{doctor.first_name} {doctor.last_name}</h2>
+                          </div>
+                          <div className="doctor-details">
+                              <p>Specialization: {doctor.specialization}</p>
+                              <p>Experience: {doctor.years_of_experience} years</p>
+                              <p>Department: {doctor.department}</p>
+                              <p>Consultation Fees: Rs. {doctor.consultation_fees}</p>
+                              <p>Biography: {doctor.biography}</p>
+                          </div>
+                      </div>
+                  </li>
               ))}
-          </div>
+          </ul>
+          <button className='add doctor' onClick={() => getDoctorinfo()}/>
       </div>
   );
 }
